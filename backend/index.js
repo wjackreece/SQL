d31f988901e46ec506937aab269cd132171fd3f7
Wynstona Jackreece <wjackreece@gmail.com>
2023-09-14 23:03:46 -0400

    Created connection to mysql

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "246958Wpjl!",
+  database: "test",
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend!");
